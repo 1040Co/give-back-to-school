7124fbd1d66556b7699bd6840408c42f37860f90
1040Co <104475218+1040Co@users.noreply.github.com>
2026-09-09 09:57:50 +0800

    Update page.tsx

--- app/giver/sign-in/page.tsx
+++ app/giver/sign-in/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { FormEvent, useState } from "react";
+import { FormEvent, useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import { createClient } from "../../../lib/supabase/client";
 import LoadingCursor from "../../LoadingCursor";
